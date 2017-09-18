207fcfbcd3591fba581e333dd27cc01c4687b4e2
Ayaan Faiz <ayaan2103@gmail.com>
2017-09-18 23:42:22 +0530

    Tree search

---
+++ treeSearch.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct node {
+    char key[100]; // can also assign memory at runtime, using malloc/calloc
+    int count;
+    struct node *left;
+    struct node *right;
+} node;
+
+node *insert(node *root, char *word) {
+
+    if (root == NULL) {
+        node *newNode = calloc(1, sizeof(node));
+        strcpy(newNode->key, word);
+        newNode->count = 1;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    if (strcmp(root->key, word) == 0)
+        root->count++;
+    else if (strcmp(root->key, word) > 0)
+        root->right = insert(root->right, word);
+    else
+        root->left = insert(root->left, word);
+    return root;
+}
+
+node *findMost(node *root) {
+    if (root == NULL || root->left == NULL && root->right == NULL)
+        return root;
+    node *left = findMost(root->left);
+    node *right = findMost(root->right);
+
+    node *larger;
+    if (left == NULL)
+        larger = right;
+    else if (right == NULL)
+        larger = left;
+    else
+        larger = (left->count > right->count) ? left : right;
+
+    return (larger->count > root->count) ? larger : root;
+}
+
+void printTree(node *root) {
+    if (root == NULL)
+        return;
+    printTree(root->left);
+    printf("%s %d\n", root->key, root->count);
+    printTree(root->right);
+}
+
+int main() {
+    node *root = NULL;
+    FILE *file = fopen("leipzig1M.txt", "r");
+    char *word = calloc(100, sizeof(char));
+    while (!feof(file)) {
+        fscanf(file, "%s", word);
+        if (strlen(word) >= 10)
+            root = insert(root, word);
+    }
+    node *large = findMost(root);
+    printf("%s %d\n", large->key, large->count);
+     printTree(root);
+}
